adea5d4abf285dc44c8174b9d990926ab73cebef
uyiplay44 <uyiplay44@hotmail.com>
2023-07-02 02:27:46 +0000

    JSX file update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Profile from "./pages/Profile";
-import Offers from "./pages/Offers";
-import {ForgetPassword} from "./pages/ForgetPassword";
+import { Home } from "./pages/Home";
+import { Profile } from "./pages/Profile";
+import { Offers } from "./pages/Offers";
+import { ForgetPassword } from "./pages/ForgetPassword";
 import { Header } from "./Components/Header";
 import { SignIn } from "./pages/SignIn";
 import { SignUp } from "./pages/SignUp";
